d8d5ff3fe898311fede17c805c363b023f1a261c
Tanuj Sahu <tanujsahu09@gmail.com>
2023-03-19 12:21:23 +0530

    refactor: some changes in code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
-
-// import 'rxjs/add/operator/toPromise';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,29 +7,17 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class AppComponent implements OnInit {
-  title = 'datatable';
-  dtOptions: any; //Promise<DataTables.Settings>
-  constructor() {
-    // this.dtOptions: ;
 
-  }
+  constructor() { }
 
-  people = [
+  cloumns = ['Id', 'firstName', 'lastName']
+  employeeList = [
     { Id: 1, firstName: "John", lastName: "Doe" },
     { Id: 2, firstName: "Jane", lastName: "Smith" },
     { Id: 3, firstName: "Bob", lastName: "Johnson" }
   ];
 
 
-  ngOnInit(): void {
-    this.dtOptions = this.people;
-    console.log(this.people);
-    console.log(this.dtOptions)
+  ngOnInit(): void { }
 
-  }
-
-  private handleError(error: any): Promise<any> {
-    console.error('An error occurred', error); // for demo purposes only
-    return Promise.reject(error.message || error);
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataTablesModule } from "angular-datatables";
+
 @NgModule({
   declarations: [
     AppComponent
